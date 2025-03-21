7ea5bb33f8c11d76f98961f93ed0b449bb24f14f
Adam Stievenard <adam.stievenard.etu>
2025-03-21 11:59:42 +0100

    ghhugihyu couleurs lalala

--- convert.sh
+++ convert.sh
@@ -7,6 +7,6 @@ pip install weasyprint
 weasyprint --info
 
 # convert to html with pandoc
-pandoc -s -c style.css rapport1.md rapport2.md rapport3.md -o rapport.html --toc -N --lot
+pandoc -s -c style.css rapport1.md rapport2.md rapport3.md -o rapport.html --toc -N
 # convert to pdf with pandoc using weasyprint as pdf engine
-pandoc -s --pdf-engine=weasyprint -c style.css rapport1.md rapport2.md rapport3.md -o rapport.pdf --toc -N --lot
+pandoc -s --pdf-engine=weasyprint -c stylePDF.css  rapport1.md rapport2.md rapport3.md -o rapport.pdf --toc -N
